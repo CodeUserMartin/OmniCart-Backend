3b831492c04e2028437797cea95e32cd5d6abfa6
CodeUserMartin <martinpatole33@gmail.com>
2026-04-28 16:28:01 +0530

    added some cart controllers

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -68,16 +68,112 @@ const addItemToCart = async (req, res) => {
 
 const updateItemFromCart = async (req, res) => {
 
+    const { productId } = req.params;
+    const { quantity } = req.body;
+    const qty = Number(quantity);
+
+    if (!productId || qty || qty < 1) {
+        throw new ApiError(400, "Invalid product or quantity!");
+    }
+
+    // Finding the user Cart
+    const cart = await Cart.findById({ userId: req.user._id });
+
+    if (!cart) {
+        throw new ApiError(404, "Cart not Found!")
+    }
+
+    // Find item in cart
+    const item = cart.items.find(
+        item => item.productId.toString() === productId
+    )
+
+    if (!item) {
+        throw new ApiError(404, "Product invalid or not found!");
+    }
+
+    // Update Quantuty count
+    item.quantity += qty
+
+    await cart.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { product },
+                "Cart updated Successfully!"
+            )
+        )
 }
 
 const deleteItemFromCart = async (req, res) => {
 
+    const { productId } = req.params;
+
+    if (!productId) {
+        throw new ApiError(400, "Failed to find or delete the Product!")
+    }
+
+    // Find user Cart
+    const cart = await Cart.findOne({ userId: req.user._id });
+
+    if (!cart) {
+        throw new ApiError(404, "Cart not Found!");
+    }
+
+    //Check if item exists FIRST
+    const item = cart.items.find(
+        item => item.productId.toString() === productId
+    )
+
+    if (!item) {
+        throw new ApiError(404, "Product Invalid or not found!")
+    }
+
+    cart.items = cart.items.filter(
+        item => item.productId.toString() !== productId
+    );
+
+    await cart.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { cart },
+                "Product removed from cart!"
+            )
+        )
 }
 
 const clearCart = async (req, res) => {
 
-}
+    const cart = await Cart.findOneAndUpdate({
+        userId: req.user._id
+    },
+        { $set: { items: [] } },
+        { returnDocument: "after" },
 
+    )
+
+    if (!cart) {
+        throw new ApiError(404, "Cart not Found!")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { cart },
+                "Cart cleared successfully!"
+            )
+        )
+
+}
 
 
 export {
