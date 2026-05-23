49c2004b754c0c9838470e5e154a9bb735abc614
CodeUserMartin <martinpatole33@gmail.com>
2026-05-23 17:27:25 +0530

    created order model and status constants

---
+++ src/constants/orderStatus.constants.js
@@ -0,0 +1,10 @@
+const orderStatusEnum = {
+    PENDING: "pending",
+    CONFIRMED: "confirmed",
+    SHIPPED: "shipped",
+    DELIVERED: "delivered",
+    CANCELLED: "cancelled",
+}
+
+export const availableOrderStatus = Object.values(orderStatusEnum); 
+
---
+++ src/models/order.models.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true,
+        },
+        items: [
+            {
+                productId: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Product",
+                },
+            }
+        ],
+        totalAmount: {
+            type: Number,
+            required: true,
+        },
+        orderStatus: {
+            type: String,
+            enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+            default: "pending",
+        },
+        paymentStatus: {
+            type: String,
+            enum: ["pending", "paid", "failed"],
+            default: "pending",
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["credit_card", "debit_card", "paypal", "cash_on_delivery"],
+        },
+        shippingAddress: {
+            addressLine: String,
+            city: String,
+            state: String,
+            pincode: String,
+            country: String,
+        },
+    },
+    { timestamps: true });
+
+
+export const Order = mongoose.model("Order", orderSchema);
