a242e510535a91d863e4eab291a741d8fe90093b
CodeUserMartin <martinpatole33@gmail.com>
2026-06-20 18:20:41 +0530

    added new order controller routes

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { getOrders, checkoutCart, buyProduct, updateOrderStatus, cancelOrder } from "../controllers/order.controllers.js";
+import { getOrders, checkoutCart, buyProduct, updateOrderStatus, cancelOrder, getSellerPendingOrders, acceptOrder, getSellerConfirmOrders, shipOrder, deliverOrder, getSellerShippedOrders, getSellerDeliveredOrders, getSellerCancelledOrders, getSellerDashboard } from "../controllers/order.controllers.js";
 
 const router = Router();
 
@@ -15,12 +15,39 @@ router.route("/checkout")
 router.route("/buy-now/:productId")
     .post(buyProduct)
 
-router.route("/cancel/:orderId")
-    .put(cancelOrder)
+router.route("/seller/pending")
+    .get(getSellerPendingOrders)
 
+router.route("/seller/accept/:itemId")
+    .put(acceptOrder);
+
+router.route("/seller/confirmed")
+    .get(getSellerConfirmOrders);
+
+router.route("/seller/ship/:itemId")
+    .put(shipOrder);
+
+router.route("/seller/shipped")
+    .get(getSellerShippedOrders);
+
+router.route("/seller/deliver/:itemId")
+    .put(deliverOrder);
+
+router.route("/seller/delivered")
+    .get(getSellerDeliveredOrders);
+
+// TODO: Refactor for item-level order status
 router.route("/:orderId/status")
     .put(updateOrderStatus)
 
+router.route("/cancel/:itemId")
+    .put(cancelOrder);
+
+router.route("/seller/cancelled")
+    .get(getSellerCancelledOrders);
+
+router.route("/seller/dashboard")
+    .get(getSellerDashboard);
 
 
 
