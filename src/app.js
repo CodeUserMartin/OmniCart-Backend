f03099bfa7b20778678bf1435333d6ee55843a53
CodeUserMartin <martinpatole33@gmail.com>
2026-06-20 18:21:11 +0530

    main import

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 dotenv.config();
 
@@ -29,12 +30,15 @@ import authRoutes from "./routes/auth.routes.js"
 import productRoutes from "./routes/product.routes.js"
 import cartRoutes from "./routes/cart.routes.js"
 import orderRoutes from "./routes/order.routes.js"
+import notificationRoutes from "./routes/notification.routes.js"
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/notification", notificationRoutes);
 
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("Welcome to OmniCart!!");
